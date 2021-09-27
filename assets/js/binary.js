a1ece1843a68d43b2fb893f600b5f6818e5cfbb9
Abhijith Chatra <abchatra@microsoft.com>
2021-09-26 18:41:29 -0700

    1.0.1

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.5.56/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"1.0.0","target":"arcade","targetVersion":"1.5.56","repo":"abchatra/pxt-mobs-test4"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.5.56/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"1.0.1","target":"arcade","targetVersion":"1.5.56","repo":"abchatra/pxt-mobs-test4"}
 // total=1138443 new=5.56% cached=88.90% other=5.53%
 (function (ectx) {
 'use strict';
@@ -384,7 +384,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P35515(s) {
+function _main___P36108(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -734,13 +734,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P35515.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P35515.continuations = [  ]
+_main___P36108.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P36108.continuations = [  ]
 
-function _main___P35515_mk(s) {
+function _main___P36108_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P35515, depth: s.depth + 1,
+        parent: s, fn: _main___P36108, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -38561,5 +38561,5 @@ const sprites_StaticObstacle__C19063_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, [])
 
-return _main___P35515
+return _main___P36108
 })
