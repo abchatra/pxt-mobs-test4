f7a359ae334a894c9ef57583ee5b7d53aed3d2d7
Abhijith Chatra <abchatra@microsoft.com>
2021-09-26 18:46:34 -0700

    Fixing snake

--- main.ts
+++ main.ts
@@ -115,73 +115,39 @@ namespace Mobs {
     }
     function spawnSnake() {
         let list = [img`
-    . . f f f . . . . . . . . f f f 
-    . f f c c . . . . . . f c b b c 
-    f f c c . . . . . . f c b b c . 
-    f c f c . . . . . . f b c c c . 
-    f f f c c . c c . f c b b c c . 
-    f f c 3 c c 3 c c f b c b b c . 
-    f f b 3 b c 3 b c f b c c b c . 
-    . c 1 b b b 1 b c b b c c c . . 
-    . c 1 b b b 1 b b c c c c . . . 
-    c b b b b b b b b b c c . . . . 
-    c b 1 f f 1 c b b b b f . . . . 
-    f f 1 f f 1 f b b b b f c . . . 
-    f f 2 2 2 2 f b b b b f c c . . 
-    . f 2 2 2 2 b b b b c f . . . . 
-    . . f b b b b b b c f . . . . . 
-    . . . f f f f f f f . . . . . . 
+    . . . . . . c c c c c c . . . . 
+    . . . . . c 6 7 7 7 7 6 c . . . 
+    . . . . c 7 7 7 7 7 7 7 7 c . . 
+    . . . c 6 7 7 7 7 7 7 7 7 6 c . 
+    . . . c 7 7 7 c 6 6 6 6 c 7 c . 
+    . . . f 7 7 7 6 f 6 6 f 6 7 f . 
+    . . . f 7 7 7 7 7 7 7 7 7 7 f . 
+    . . c f 6 7 7 c 6 7 7 7 7 f . . 
+    . c 7 7 f 6 7 7 c c c c f . . . 
+    c 7 7 7 7 f c 6 7 7 7 2 7 c . . 
+    c c 6 7 7 6 c f c 7 7 2 7 7 c . 
+    . . c 6 6 6 c c f 6 7 1 1 1 1 c 
+    . . f 6 6 6 6 c 6 6 1 1 1 1 1 f 
+    . . f c 6 6 6 6 6 1 1 1 1 1 6 f 
+    . . . f 6 6 6 1 1 1 1 1 1 6 f . 
+    . . . . f c c c c c c c c c . . 
     `, img`
-    . . f f f . . . . . . . . . . . 
-    f f f c c . . . . . . . . f f f 
-    f f c c c . c c . . . f c b b c 
-    f f c 3 c c 3 c c f f b b b c . 
-    f f c 3 b c 3 b c f b b c c c . 
-    f c b b b b b b c f b c b c c . 
-    c c 1 b b b 1 b c b b c b b c . 
-    c b b b b b b b b b c c c b c . 
-    c b 1 f f 1 c b b c c c c c . . 
-    c f 1 f f 1 f b b b b f c . . . 
-    f f f f f f f b b b b f c . . . 
-    f f 2 2 2 2 f b b b b f c c . . 
-    . f 2 2 2 2 2 b b b c f . . . . 
-    . . f 2 2 2 b b b c f . . . . . 
-    . . . f f f f f f f . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . c c . c c . . . . . . . . 
-    . . f 3 c c 3 c c c . . . . . . 
-    . f c 3 b c 3 b c c c . . . . . 
-    f c b b b b b b b b f f . . . . 
-    c c 1 b b b 1 b b b f f . . . . 
-    c b b b b b b b b c f f f . . . 
-    c b 1 f f 1 c b b f f f f . . . 
-    f f 1 f f 1 f b c c b b b . . . 
-    f f f f f f f b f c c c c . . . 
-    f f 2 2 2 2 f b f b b c c c . . 
-    . f 2 2 2 2 2 b c c b b c . . . 
-    . . f 2 2 2 b f f c c b b c . . 
-    . . . f f f f f f f c c c c c . 
-    . . . . . . . . . . . . c c c c 
-    `, img`
-    . f f f . . . . . . . . f f f . 
-    f f c . . . . . . . f c b b c . 
-    f c c . . . . . . f c b b c . . 
-    c f . . . . . . . f b c c c . . 
-    c f f . . . . . f f b b c c . . 
-    f f f c c . c c f b c b b c . . 
-    f f f c c c c c f b c c b c . . 
-    . f c 3 c c 3 b c b c c c . . . 
-    . c b 3 b c 3 b b c c c c . . . 
-    c c b b b b b b b b c c . . . . 
-    c 1 1 b b b 1 1 b b b f c . . . 
-    f b b b b b b b b b b f c c . . 
-    f b c b b b c b b b b f . . . . 
-    . f 1 f f f 1 b b b c f . . . . 
-    . . f b b b b b b c f . . . . . 
-    . . . f f f f f f f . . . . . . 
+    . . . . . . . c c c c c c . . . 
+    . . . . . . c 6 7 7 7 7 6 c . . 
+    . . . . . c 7 7 7 7 7 7 7 7 c . 
+    . . . . c 6 7 7 7 7 7 7 7 7 6 c 
+    . . . . c 7 7 7 c 6 6 6 6 c 7 c 
+    . . . . f 7 7 7 6 f 6 6 f 6 7 f 
+    . . . . f 7 7 7 7 7 7 7 7 7 7 f 
+    . . . . f 6 7 7 c 6 7 7 7 7 f . 
+    . . c c c f 6 7 7 c c c c f . . 
+    . c 7 7 7 c c f 7 7 7 2 6 c . . 
+    c 7 7 7 7 6 f c 7 7 2 7 7 6 c . 
+    c c c 6 6 6 c 6 6 7 1 1 1 1 c . 
+    . . c 6 6 6 6 6 6 1 1 1 1 1 c . 
+    . . c 6 6 6 6 6 1 1 1 1 1 6 c . 
+    . . c c 6 6 7 1 1 1 1 1 6 c . . 
+    . . . c c c c c c c c c c . . . 
     `]
         return spawn(list);
     }
