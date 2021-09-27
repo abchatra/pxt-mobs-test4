7e84e676f23d7e0997ce26e24456a8e4dac5bc16
Abhijith Chatra <abchatra@microsoft.com>
2021-09-26 18:37:11 -0700

    Updating with more mobs

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 enum Mob {
     //% block="duck"
     duck,
+    //% block="snake"
+    snake,
     //% block="monkey"
     moneky,
     //% block="dog"
@@ -35,10 +37,295 @@ namespace Mobs {
             200,
             true
         )
-        mySprite.setVelocity(44, 31)
+        mySprite.setVelocity(Math.randomRange(0, 60), Math.randomRange(0, 50))
         mySprite.setBounceOnWall(true)
         return mySprite
     }
+    function spawnSnake() {
+       let list = [img`
+    . . f f f . . . . . . . . f f f 
+    . f f c c . . . . . . f c b b c 
+    f f c c . . . . . . f c b b c . 
+    f c f c . . . . . . f b c c c . 
+    f f f c c . c c . f c b b c c . 
+    f f c 3 c c 3 c c f b c b b c . 
+    f f b 3 b c 3 b c f b c c b c . 
+    . c 1 b b b 1 b c b b c c c . . 
+    . c 1 b b b 1 b b c c c c . . . 
+    c b b b b b b b b b c c . . . . 
+    c b 1 f f 1 c b b b b f . . . . 
+    f f 1 f f 1 f b b b b f c . . . 
+    f f 2 2 2 2 f b b b b f c c . . 
+    . f 2 2 2 2 b b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    `, img`
+    . . f f f . . . . . . . . . . . 
+    f f f c c . . . . . . . . f f f 
+    f f c c c . c c . . . f c b b c 
+    f f c 3 c c 3 c c f f b b b c . 
+    f f c 3 b c 3 b c f b b c c c . 
+    f c b b b b b b c f b c b c c . 
+    c c 1 b b b 1 b c b b c b b c . 
+    c b b b b b b b b b c c c b c . 
+    c b 1 f f 1 c b b c c c c c . . 
+    c f 1 f f 1 f b b b b f c . . . 
+    f f f f f f f b b b b f c . . . 
+    f f 2 2 2 2 f b b b b f c c . . 
+    . f 2 2 2 2 2 b b b c f . . . . 
+    . . f 2 2 2 b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . c c . c c . . . . . . . . 
+    . . f 3 c c 3 c c c . . . . . . 
+    . f c 3 b c 3 b c c c . . . . . 
+    f c b b b b b b b b f f . . . . 
+    c c 1 b b b 1 b b b f f . . . . 
+    c b b b b b b b b c f f f . . . 
+    c b 1 f f 1 c b b f f f f . . . 
+    f f 1 f f 1 f b c c b b b . . . 
+    f f f f f f f b f c c c c . . . 
+    f f 2 2 2 2 f b f b b c c c . . 
+    . f 2 2 2 2 2 b c c b b c . . . 
+    . . f 2 2 2 b f f c c b b c . . 
+    . . . f f f f f f f c c c c c . 
+    . . . . . . . . . . . . c c c c 
+    `, img`
+    . f f f . . . . . . . . f f f . 
+    f f c . . . . . . . f c b b c . 
+    f c c . . . . . . f c b b c . . 
+    c f . . . . . . . f b c c c . . 
+    c f f . . . . . f f b b c c . . 
+    f f f c c . c c f b c b b c . . 
+    f f f c c c c c f b c c b c . . 
+    . f c 3 c c 3 b c b c c c . . . 
+    . c b 3 b c 3 b b c c c c . . . 
+    c c b b b b b b b b c c . . . . 
+    c 1 1 b b b 1 1 b b b f c . . . 
+    f b b b b b b b b b b f c c . . 
+    f b c b b b c b b b b f . . . . 
+    . f 1 f f f 1 b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    `]
+    return list;
+    }
+    function spawnBat() {
+        let list = [img`
+    . . f f f . . . . . . . . f f f 
+    . f f c c . . . . . . f c b b c 
+    f f c c . . . . . . f c b b c . 
+    f c f c . . . . . . f b c c c . 
+    f f f c c . c c . f c b b c c . 
+    f f c 3 c c 3 c c f b c b b c . 
+    f f b 3 b c 3 b c f b c c b c . 
+    . c 1 b b b 1 b c b b c c c . . 
+    . c 1 b b b 1 b b c c c c . . . 
+    c b b b b b b b b b c c . . . . 
+    c b 1 f f 1 c b b b b f . . . . 
+    f f 1 f f 1 f b b b b f c . . . 
+    f f 2 2 2 2 f b b b b f c c . . 
+    . f 2 2 2 2 b b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    `, img`
+    . . f f f . . . . . . . . . . . 
+    f f f c c . . . . . . . . f f f 
+    f f c c c . c c . . . f c b b c 
+    f f c 3 c c 3 c c f f b b b c . 
+    f f c 3 b c 3 b c f b b c c c . 
+    f c b b b b b b c f b c b c c . 
+    c c 1 b b b 1 b c b b c b b c . 
+    c b b b b b b b b b c c c b c . 
+    c b 1 f f 1 c b b c c c c c . . 
+    c f 1 f f 1 f b b b b f c . . . 
+    f f f f f f f b b b b f c . . . 
+    f f 2 2 2 2 f b b b b f c c . . 
+    . f 2 2 2 2 2 b b b c f . . . . 
+    . . f 2 2 2 b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . c c . c c . . . . . . . . 
+    . . f 3 c c 3 c c c . . . . . . 
+    . f c 3 b c 3 b c c c . . . . . 
+    f c b b b b b b b b f f . . . . 
+    c c 1 b b b 1 b b b f f . . . . 
+    c b b b b b b b b c f f f . . . 
+    c b 1 f f 1 c b b f f f f . . . 
+    f f 1 f f 1 f b c c b b b . . . 
+    f f f f f f f b f c c c c . . . 
+    f f 2 2 2 2 f b f b b c c c . . 
+    . f 2 2 2 2 2 b c c b b c . . . 
+    . . f 2 2 2 b f f c c b b c . . 
+    . . . f f f f f f f c c c c c . 
+    . . . . . . . . . . . . c c c c 
+    `, img`
+    . f f f . . . . . . . . f f f . 
+    f f c . . . . . . . f c b b c . 
+    f c c . . . . . . f c b b c . . 
+    c f . . . . . . . f b c c c . . 
+    c f f . . . . . f f b b c c . . 
+    f f f c c . c c f b c b b c . . 
+    f f f c c c c c f b c c b c . . 
+    . f c 3 c c 3 b c b c c c . . . 
+    . c b 3 b c 3 b b c c c c . . . 
+    c c b b b b b b b b c c . . . . 
+    c 1 1 b b b 1 1 b b b f c . . . 
+    f b b b b b b b b b b f c c . . 
+    f b c b b b c b b b b f . . . . 
+    . f 1 f f f 1 b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    `]
+    return list;
+    }
+
+    function spawnDog() {
+        let list = [img`
+    . . 4 4 4 . . . . 4 4 4 . . . . 
+    . 4 5 5 5 e . . e 5 5 5 4 . . . 
+    4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
+    4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
+    e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
+    . e e 5 5 5 5 5 5 5 5 e e . . . 
+    . . e 5 f 5 5 5 5 f 5 e . . . . 
+    . . f 5 5 5 4 4 5 5 5 f . . f f 
+    . . f 4 5 5 f f 5 5 6 f . f 5 f 
+    . . . f 6 6 6 6 6 6 4 4 f 5 5 f 
+    . . . f 4 5 5 5 5 5 5 4 4 5 f . 
+    . . . f 5 5 5 5 5 4 5 5 f f . . 
+    . . . f 5 f f f 5 f f 5 f . . . 
+    . . . f f . . f f . . f f . . . 
+    `, img`
+    . . . . . . . . . . . . . . . . 
+    . . 4 4 4 . . . . 4 4 4 . . . . 
+    . 4 5 5 5 e . . e 5 5 5 4 . . . 
+    4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
+    4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
+    e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
+    . e e 5 5 5 5 5 5 5 5 e e . . . 
+    . . e 5 f 5 5 5 5 f 5 e . . . . 
+    . . f 5 5 5 4 4 5 5 5 f . f f . 
+    . . . 4 5 5 f f 5 5 6 f f 5 f . 
+    . . . f 6 6 6 6 6 6 4 4 4 5 f . 
+    . . . f 5 5 5 5 5 5 5 f f f . . 
+    . . . f 5 4 5 f f f 5 f . . . . 
+    . . . f f f f f . . f f . . . . 
+    `, img`
+    . . . . . . . . . . . . . . . . 
+    . . 4 4 4 . . . . 4 4 4 . . . . 
+    . 4 5 5 5 e . . e 5 5 5 4 . . . 
+    4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
+    4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
+    e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
+    . e e 5 5 5 5 5 5 5 5 e e . . . 
+    . . e 5 f 5 5 5 5 f 5 e . . . . 
+    . . f 5 5 5 4 4 5 5 5 f . f f . 
+    . . . 4 5 5 f f 5 5 6 f f 5 f . 
+    . . . f 6 6 6 6 6 6 4 f 5 5 f . 
+    . . . f 5 5 5 5 5 5 5 4 5 f . . 
+    . . . . f 5 4 5 f 5 f f f . . . 
+    . . . . . f f f f f f f . . . . 
+    `]
+        return spawn(list);
+    }
+
+    function spawnMonkey() {
+        let list = [img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . f f 
+    c c c c c d d d e e f c . f e f 
+    . f d d d d d e e f f . . f e f 
+    . . f f f f f e e e e f . f e f 
+    . . . . f e e e e e e e f f e f 
+    . . . f e f f e f e e e e f f . 
+    . . . f e f f e f e e e e f . . 
+    . . . f d b f d b f f e f . . . 
+    . . . f d d c d d b b d f . . . 
+    . . . . f f f f f f f f f . . . 
+    `, img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    c d e e d d d d e e d d f . . . 
+    c d d d d c d d e e b d c . . . 
+    c c c c c d d e e e b d c . f f 
+    . f d d d d e e e f f c . f e f 
+    . f f f f f f e e e e f . f e f 
+    . f f f f e e e e e e e f f e f 
+    f d d f d d f e f e e e e f f . 
+    f d b f d b f e f e e e e f . . 
+    f f f f f f f f f f f f e f . . 
+    . . . . . . . . . f c d d f . . 
+    . . . . . . . . . . f f f f . . 
+    `, img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f f . . . . 
+    . c d d d d d d e e d d f . . . 
+    . c d f d d f d e e b d c . . . 
+    c d d f d d f d e e b d c . f f 
+    c d e e d d d d e e f c . f e f 
+    c d d d d c d e e e f . . f e f 
+    . f c c c d e e e f f . . f e f 
+    . . f f f f f e e e e f . f e f 
+    . . . . f e e e e e e e f f f . 
+    . . f f e f e e f e e e e f . . 
+    . f e f f e e f f f e e e f . . 
+    f d d b d d c f f f f f f b f . 
+    f d d c d d d f . . f c d d f . 
+    . f f f f f f f . . . f f f . . 
+    `, img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f f f . . . . 
+    . . f d d d e e e e d d f . . . 
+    . c d d d d d e e e b d c . . . 
+    . c d d d d d d e e b d c . . . 
+    c d d f d d f d e e f c . f f . 
+    c d d f d d f d e e f . . f e f 
+    c d e e d d d d e e f . . f e f 
+    . f d d d c d e e f f . . f e f 
+    . . f f f d e e e e e f . f e f 
+    . . . . f e e e e e e e f f f . 
+    . . . . f f e e e e e b f f . . 
+    . . . f e f f e e c d d f f . . 
+    . . f d d b d d c f f f . . . . 
+    . . f d d c d d d f f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    `, img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . . . 
+    c c c c c d d e e e f c . . . . 
+    . f d d d d e e e f f . . . . . 
+    . . f f f f f e e e e f . . . . 
+    . . . . f f e e e e e e f . f f 
+    . . . f e e f e e f e e f . e f 
+    . . f e e f e e f e e e f . e f 
+    . f b d f d b f b b f e f f e f 
+    . f d d f d d f d d b e f f f f 
+    . . f f f f f f f f f f f f f . 
+    `]
+        return spawn(list);
+    }
+
     function spawnDuck() {
         let list = [img`
     . . . . . . . . . . . . . . . . 
@@ -150,9 +437,11 @@ namespace Mobs {
     export function spawnMob(mob: Mob) {
         switch (mob) {
             case Mob.duck: return spawnDuck();
-            case Mob.duck: return spawnDuck();
-
+            case Mob.dog: return spawnDog();
+            case Mob.snake: return spawnSnake();
+            case Mob.moneky: return spawnMonkey();
         }
         return spawnDuck();
     }
 }
+
